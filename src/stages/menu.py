492584f5e22d9159ce16ee515da4f5a6de3bac0c
sammy-hutch <sam-hutch25@hotmail.co.uk>
2025-04-12 18:34:04 +0200

    feat: add resume game functionality

--- src/stages/menu.py
+++ src/stages/menu.py
@@ -5,6 +5,11 @@ from components.sprite import Sprite
 
 def new_game():
     from core.engine import engine
+    engine.lvl_num = 1
+    engine.switch_to("Play")
+
+def resume_game():
+    from core.engine import engine
     engine.switch_to("Play")
 
 def quit_game():
@@ -17,23 +22,30 @@ def menu():
 
     # Create buttons
     new_game_button = Entity(Label("RedRose-Regular.ttf", 
-                                   "New Game", 70, 
+                                   "New Game", 50, 
                                    (150, 0, 0)))
+    resume_game_button = Entity(Label("RedRose-Regular.ttf",
+                                      "Resume Game", 50,
+                                      (150, 0, 0)))
     quit_game_button = Entity(Label("RedRose-Regular.ttf", 
-                                    "Quit Game", 70, 
+                                    "Quit Game", 50, 
                                     (150, 0, 0)))
     
     # Set button size
     new_game_button_size = new_game_button.get(Label).get_bounds()
+    resume_game_button_size = resume_game_button.get(Label).get_bounds()
     quit_game_button_size = quit_game_button.get(Label).get_bounds()
 
     # Set button position
     from core.camera import camera
     new_game_button.x = camera.width/2 - new_game_button_size.width/2 - 275
     new_game_button.y = camera.height - 750
+    resume_game_button.x = camera.width/2 - resume_game_button_size.width/2 - 275
+    resume_game_button.y = camera.height - 675
     quit_game_button.x = camera.width/2 - quit_game_button_size.width/2 - 275
-    quit_game_button.y = camera.height - 650
+    quit_game_button.y = camera.height - 600
 
     # Add button functionality
     new_game_button.add(Button(new_game, new_game_button_size))
+    resume_game_button.add(Button(resume_game, resume_game_button_size))
     quit_game_button.add(Button(quit_game, quit_game_button_size))
